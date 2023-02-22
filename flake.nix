fd42b4adc4936542039aba96fd30d6f81335c0c2
Erik Rodriguez <erik@rodgz.com>
2023-02-22 13:11:30 -0300

    flake.{nix,lock}: Use nixos-unstable and update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "histop";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
